f4a537fea135d0e6c3c5cfde0a9d035d739e6dde
Rotary <zhouxuan@jkweixin.com>
2018-12-10 16:08:42 +0800

    ui规范

--- src/components/navigation-bar/navigation-bar.vue
+++ src/components/navigation-bar/navigation-bar.vue
@@ -4,7 +4,7 @@
       <div class="status-bar" :style="{height: statusBarHeight + 'px'}"></div>
       <div class="head-content" :style="{color: titleColor}">{{currentTitle}}</div>
       <div class="head-arrow" v-if="showArrow" @click="goBackUrl">
-        <img v-if="imageUrl" :src="imageUrl + '/zd-image/1.2/icon-title_back@2x.png'" class="head-arrow-img">
+        <img v-if="imageUrl" :src="imageUrl + arrowUrl" class="head-arrow-img">
       </div>
     </div>
     <div v-if="!translucent" :style="{height: statusBarHeight + 44 + 'px'}"></div>
@@ -14,10 +14,12 @@
   /* eslint-disable no-undef */
   import wx from 'wx'
   import app from '@src/main'
+
   function pageRouter() {
     let page = app.config.pages.find(item => /\^/.test(item))
     return page.replace('^', '/')
   }
+
   let DEFAULT_PAGE = pageRouter()
   export default {
     name: 'HEAD_ITEM',
@@ -56,12 +58,16 @@
       hasTranslucentFn: {
         type: Boolean,
         default: false
+      },
+      arrowUrl: {
+        type: String,
+        default: '/zd-image/1.2/icon-title_back@2x.png'
       }
     },
     onPageScroll(e) {
       this._diyHeadNavigation(e)
     },
-    data () {
+    data() {
       return {
         statusBarHeight: 20,
         translucentTitle: this.title
@@ -144,10 +150,10 @@
       align-items: center
       &:after
         content: ''
-        position :absolute
-        width :100%
-        height :100%
-        padding :12px 20px
+        position: absolute
+        width: 100%
+        height: 100%
+        padding: 12px 20px
       .head-arrow-img
         width: 18px
         height: @width
