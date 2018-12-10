823e9ef15741489ce9b101a9ab2601861fd455a6
wengtianliang <wengtianliang@jkweixin.com>
2018-12-10 18:04:52 +0800

    save xoe

---
+++ src/pages/choiceness/choiceness.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="choiceness">
+    <div class="choiceness-top">
+      <div class="group-info">
+        <div class="info-box">
+          <div class="info-box-top">11</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const PAGE_NAME = 'CHOICENESS'
+
+  export default {
+    name: PAGE_NAME,
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+  .choiceness-top
+    position: relative
+    padding-top: 17vw
+    .group-info
+      padding: 0 3.2vw
+      box-sizing: border-box
+      .info-box
+        layout(row)
+        align-items: center
+        justify-content: center
+        height: 96px
+        background: #fff
+        border-radius: 6px
+        box-shadow: 0 3px 10px 0 rgba(17,17,17,0.06)
+        .info-box-top
+          layout(row)
+          align-items: center
+  .choiceness
+    width: 100%
+</style>
