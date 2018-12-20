8607b4796ce1031075566fcb9853b34ea85b2ce6
Rotary <zhouxuan@jkweixin.com>
2018-12-20 16:08:47 +0800

    新增

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,21 +6,21 @@ const DEV = {
   api: 'http://social-shopping-api.jerryf.cn',
   image: 'https://img.jerryf.cn/defaults',
   upload: 'http://social-shopping-api.jerryf.cn',
-  imRecord: 'https://data-analysis-api.jkweixin.net'
+  outLink: 'https://fresh-h5.jkweixin.net' // 外部h5链接
 }
 
 const TEST = {
   api: 'https://social-shopping-api.jkweixin.net',
   image: 'https://img.jkweixin.net/defaults',
   upload: 'https://social-shopping-api.jkweixin.net',
-  imRecord: 'https://data-analysis-api.jkweixin.net'
+  outLink: 'https://fresh-h5.jkweixin.net'
 }
 
 const PROD = {
   api: 'https://social-shopping-api.jkweixin.com/' + version,
   image: 'https://img.jkweixin.com/defaults',
   upload: 'https://social-shopping-api.jkweixin.com/' + version,
-  imRecord: 'https://data-analysis-api.jkweixin.com'
+  outLink: 'https://fresh-h5.jkweixin.com'
 }
 
 export const baseURL = env === 'production' ? PROD : env === 'test' ? TEST : DEV
