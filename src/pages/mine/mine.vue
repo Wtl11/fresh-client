91447348a05761187b03ba779a4772812548f317
zhuwenping <zhuwenping@jkweixin.net>
2018-12-20 22:22:27 +0800

    我的小红点

--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -16,7 +16,7 @@
       <div class="order-item" v-for="(item, index) in orderNav" :key="index" @click="jumpOrder(item)">
         <div class="icon"><img class="icon-img" v-if="imageUrl" :src="imageUrl+item.icon_url" alt=""></div>
         <div class="txt">{{item.name}}</div>
-        <div class="mark" v-if="(index === 0 || index === 1 || index === 3) && item.count > 0">{{item.count > 99 ? 99 : item.count}}</div>
+        <div class="mark" v-if="(index === 0 || index === 1) && item.count > 0">{{item.count > 99 ? 99 : item.count}}</div>
       </div>
       <div class="arr-order">
         <img class="eor-img" v-if="imageUrl" :src="imageUrl+'/yx-image/cart/mydivision@2x.png'" alt="">
@@ -109,7 +109,6 @@
     onShow() {
       this._getShopDetail()
       this._getOrderCount()
-      this._getAfterOrderCount()
     },
     computed: {
       ...oauthComputed
@@ -205,17 +204,6 @@
                 this.orderNav[1].count = item.count
               }
             })
-          })
-      },
-      _getAfterOrderCount() {
-        API.Mine.getAfterOrderCount()
-          .then((res) => {
-            console.log(res)
-            this.$wechat.hideLoading()
-            if (res.error !== this.$ERR_OK) {
-              return
-            }
-            this.orderNav[3].count = res.data.count
           })
       },
       _getShopDetail() {
