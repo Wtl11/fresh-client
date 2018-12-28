070fd4a6bc3b030283e3a72d9f478dd2f23f9f44
wengtianliang <wengtianliang@jkweixin.com>
2018-12-28 17:54:37 +0800

    'wallet'

---
+++ src/components/wallet-info/wallet-info.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="wallet-info">
+    <div class="info-left">
+      <img class="info-left-img">
+    </div>
+    <div class="info-right">
+      <div class="info-text-box">
+        <div class="info-text-title">张三丰购买了 40斤新鲜柠檬</div>
+        <div class="info-text-bottom">
+          <div class="label-btn label-btn-active">佣金返还</div>
+          <div class="info-time">2018-06-05 10:22:50</div>
+        </div>
+      </div>
+      <div class="info-money-box">
+        <div class="money-number">+5.00</div>
+        <div class="money-balance">余额 ￥1000.00</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const COMPONENT_NAME = 'WALLET_INFO'
+
+  export default {
+    name: COMPONENT_NAME,
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+
+  .wallet-info
+    width: 100%
+    layout(row)
+    align-items: center
+    .info-left
+      width: 40px
+      height: 40px
+      margin-right: 10px
+      .info-left-img
+        width: 100%
+        height: 100%
+        display: block
+        background: #73C200
+        border-radius: 50%
+    .info-right
+      flex: 1
+      padding: 17px 0 15px
+      border-bottom-1px($color-line)
+      layout(row)
+      align-items: center
+      justify-content: space-between
+      padding-right: 15px
+      box-sizing: border-box
+      .info-text-title
+        font-size: $font-size-14
+        font-family: $font-family-regular
+        color: $color-text-main
+        width: 190px
+        margin-bottom: 8px
+        no-wrap()
+      .info-text-bottom
+        layout(row)
+        align-items: center
+        .label-btn
+          font-size: $font-size-10
+          color: #616161
+          font-family: $font-family-regular
+          height: 14px
+          line-height: 14.5px
+          padding: 0 5px
+          border-1px(#616161, 8.5px)
+          margin-right: 5px
+        .label-btn-active
+          color: $color-money
+          background: rgba(255,131,0,0.16)
+          border-1px($color-money, 8.5px)
+        .info-time
+          font-size: $font-size-11
+          color: $color-text-assist
+          font-family: $font-family-regular
+      .info-money-box
+        text-align: right
+      .money-number
+        font-size: $font-size-16
+        color: $color-money
+        font-family: $font-family-medium
+        margin-bottom: 8px
+      .money-balance
+        font-size: $font-size-14
+        color: $color-text-assist
+        font-family: $font-family-regular
+  .z
+    width: 1px
+</style>
---
+++ src/pages/group-wallet/group-wallet.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="group-wallet">
+    <navigation-bar title="团长钱包"></navigation-bar>
+    <div class="wallet-top">
+      <img class="bgimg-url" mode="aspectFill" v-if="imageUrl" :src="imageUrl + '/yx-image/wallet/icon-money_bg@2x.png'">
+      <div class="wallet-box">
+        <div class="wallet-box-left">
+          <div class="wallet-box-left-name">钱包余额(元)</div>
+          <div class="wallet-box-left-number">2859.00</div>
+        </div>
+        <div class="wallet-box-right">提现</div>
+      </div>
+      <div class="wallet-bottom">
+        <div class="wallet-bottom-main">
+          <div class="wallet-main-text">
+            <div class="jump-text">收入:￥</div>
+            <div class="jump-money">200.00</div>
+            <img class="jump-arrows" mode="aspectFill" v-if="imageUrl" :src="imageUrl + '/yx-image/cart/icon-pressed@2x.png'">
+          </div>
+          <div class="wallet-main-text">
+            <div class="jump-text">收入:￥</div>
+            <div class="jump-money">200.00</div>
+            <img class="jump-arrows" mode="aspectFill" v-if="imageUrl" :src="imageUrl + '/yx-image/cart/icon-pressed@2x.png'">
+          </div>
+          <div class="wallet-line"></div>
+        </div>
+      </div>
+    </div>
+    <div class="wallet-list">
+      <wallet-info></wallet-info>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import NavigationBar from '@components/navigation-bar/navigation-bar'
+  import WalletInfo from '@components/wallet-info/wallet-info'
+
+  const PAGE_NAME = 'GROUP_WALLET'
+
+  export default {
+    name: PAGE_NAME,
+    data() {
+      return {
+
+      }
+    },
+    components: {
+      NavigationBar,
+      WalletInfo
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+
+  .group-wallet
+    width: 100%
+  .wallet-top
+    height: 34vw
+    width: 100%
+    position: relative
+    margin-bottom: 40px
+    .bgimg-url
+      width: 100%
+      height: 100%
+      display: block
+      position: absolute
+      z-index: 1
+      left: 0
+      top: 0
+    .wallet-box
+      padding: 6.8vw 20px 0 27px
+      box-sizing: border-box
+      position: relative
+      z-index: 2
+      layout(row)
+      align-items: center
+      justify-content: space-between
+      .wallet-box-left-name
+        font-size: $font-size-14
+        color: $color-white
+        font-family: $font-family-regular
+        margin-bottom: 8px
+      .wallet-box-left-number
+        font-size: 30px
+        color: $color-white
+        font-family: $font-family-din-bold
+      .wallet-box-right
+        width: 70px
+        text-align: center
+        height: 24px
+        line-height: 24px
+        font-size: $font-size-14
+        color: $color-text-sub
+        font-family: $font-family-regular
+        background: $color-white
+        border-radius: 15px
+    .wallet-bottom
+      position: absolute
+      bottom: -30px
+      left: 0
+      padding: 0 15px
+      height: 60px
+      box-sizing: border-box
+      width: 100%
+      z-index: 2
+      .wallet-bottom-main
+        position: relative
+        width: 100%
+        height: 60px
+        border-radius: 4px
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.06)
+        background: $color-white
+        align-items: center
+        layout(row)
+        .wallet-main-text
+          width: 50%
+          height: 100%
+          layout(row)
+          align-items: center
+          justify-content: center
+          .jump-text
+            font-size: $font-size-14
+            font-family: $font-family-regular
+            color: $color-text-assist
+          .jump-money
+            font-size: $font-size-16
+            font-family: $font-family-regular
+            color: $color-text-main
+          .jump-arrows
+            display: block
+            width: 7.5px
+            height: 12.5px
+            margin-left: 4px
+        .wallet-line
+          position: absolute
+          left: 50%
+          top: -44.5px
+          width: 1px
+          height: 27px
+          border-right-1px(#E6E6E6)
+  .wallet-list
+    padding-left: 15px
+    box-sizing: border-box
+</style>
