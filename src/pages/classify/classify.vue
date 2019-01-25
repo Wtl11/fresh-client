c0569378be76936a6c2f75f9709f4ba7f313985d
wengtianliang <wengtianliang@jkweixin.com>
2019-01-25 16:09:05 +0800

    'classify'

---
+++ src/pages/classify/classify.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="classify">
+    <navigation-bar title="分类"></navigation-bar>
+    <scroll-view class="scroll-view2" v-if="tabList1.length" id="scrollView" :scroll-into-view="viewToItem" scroll-x scroll-with-animation>
+      <div class="under-line" :style="{left: move + 'px', width: arrWidth[tabIndex] + 'px' }"></div>
+      <div v-for="(item, index) in tabList1" :class="tabIndex === index ? 'item-active'  : ''" :key="index" class="item" :id="'item'+index" @click="_changeTab(index, item.id, $event)">
+      {{item.name}}
+      </div>
+    </scroll-view>
+    <div class="goods-list">
+      <div class="goods-item-box">
+        <div class="classify-box">
+          <div class="classify-box-top">
+            <img v-if="imageUrl" :src="imageUrl + '/yx-image/goods/pic-label2@2x.png'" alt="" class="top-label">
+          </div>
+          <div class="classify-box-bottom">
+            <div class="title">超值特惠 4斤新鲜柠檬4斤新鲜柠檬</div>
+            <div class="classify-price-box">
+              <div class="price-left">
+                <div class="price-number">10.8</div>
+                <div class="price-money">元</div>
+                <div class="price-line">12元</div>
+              </div>
+              <div class="price-right">
+                <img v-if="imageUrl" :src="imageUrl + '/yx-image/goods/icon-shopcart2@2x.png'" alt="" class="price-right-img">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import NavigationBar from '@components/navigation-bar/navigation-bar'
+
+  const PAGE_NAME = 'CLASSIFY'
+
+  export default {
+    name: PAGE_NAME,
+    watch: {
+      tabList(news) {
+        this.tabList1 = news
+        setTimeout(() => {
+          this.getWidth('', '', '', false)
+        }, 100)
+      },
+      infoTabIndex(newVal) {
+        this.tabIndex = newVal
+      }
+    },
+    data() {
+      return {
+        tabList: [{name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}],
+        tabList1: [{name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}, {name: '日常用品'}],
+        arrWidth: [],
+        width: 0,
+        move: 0,
+        tabIndex: 0,
+        viewToItem: 'item0'
+      }
+    },
+    components: {
+      NavigationBar
+    },
+    methods: {
+      getWidth(index, id, e) {
+        this.allWidth = 0
+        let query = wx.createSelectorQuery()
+        query.selectAll('.item').boundingClientRect()
+        query.exec((res) => {
+          this.arrWidth = res[0].map((item, index) => {
+            this.allWidth += item.width
+            return item.width
+          })
+        })
+      },
+      async _changeTab(index, id, e) {
+        let number = index * 1 === 0 ? 1 : index
+        if (this.tabIndex > index) {
+          if (index <= 3) {
+            number = 0
+          } else {
+            number = index
+          }
+        } else if (this.tabIndex < index) {
+          if (index <= 3) {
+            number = 0
+          } else {
+            number = index
+          }
+        }
+        this.viewToItem = `item${number}`
+        this.tabIndex = index
+        this.move = e.target.offsetLeft
+        this.sheTag_id = id
+        this.getGoodsList()
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+
+  .classify
+    width: 100%
+    height: 100vh
+    background: $color-background
+  .goods-list
+    layout(row)
+    align-items: center
+    .goods-item-box
+      width: 50%
+      box-sizing: border-box
+      margin-bottom: 2px
+      .classify-box
+        background: #fff
+        .classify-box-top
+          width: 49.73vw
+          height: 49.73vw
+          background: #333
+          position: relative
+          .top-label
+            position: absolute
+            left: 8px
+            top: 0
+            display: block
+            width: 31.2px
+            height: 31.4px
+        .classify-box-bottom
+          padding: 10px 12px 15px 7.5px
+          .title
+            color: $color-text-main
+            font-size: $font-size-14
+            font-family: $font-family-regular
+            no-wrap()
+            margin-bottom: 8.5px
+          .classify-price-box
+            layout(row)
+            align-items: center
+            justify-content: space-between
+            .price-left
+              layout(row)
+              align-items: center
+              .price-number
+                color: $color-money
+                font-size: $font-size-18
+                font-family: $font-family-medium
+                margin-right: 2px
+              .price-money
+                color: $color-money
+                font-size: $font-size-12
+                font-family: $font-family-medium
+                margin-right: 5px
+              .price-line
+                color: $color-text-assist
+                font-size: $font-size-12
+                font-family: $font-family-regular
+                text-decoration: line-through
+            .price-right
+              width: 23px
+              height: 23px
+              position: relative
+              .price-right-img
+                width: 23px
+                height: 23px
+                display: block
+    .goods-item-box:nth-of-type(odd)
+      padding-right: 2px
+    .goods-item-box:nth-of-type(even)
+      padding-left: 2px
+  .scroll-view2
+    display: block
+    margin: 0 auto 5px
+    height: 40px
+    width: 100vw
+    background: $color-white
+    box-shadow: 0 1px 8px 0 rgba(55, 75, 99, 0.04)
+    white-space: nowrap
+    box-sizing: border-box
+    transform: translateX(0)
+    position: relative
+    transition: all 0.3s
+    ::-webkit-scrollbar
+      width: 0
+      height: 0
+      color: transparent
+    .item
+      height: 100%
+      line-height: 40px
+      white-space: nowrap
+      padding: 0 12.5px
+      font-family: $font-family-regular
+      font-size: $font-size-14
+      color: $color-text-main
+      text-align: center
+      display: inline-block
+      position: relative
+      /*transition: all 0.3s*/
+      min-width: 76px
+      box-sizing: border-box
+      transform-origin: 50%
+    .item-active
+      font-family: $font-family-medium
+      font-size: $font-size-16
+
+  .under-line
+    position: absolute
+    bootom: 0
+    left: 0
+    width: 76px
+    background: $color-main
+    /*transition: left 0.3s*/
+    height: 33px
+    border-radius: 8px 8px 0px 0px
+
+</style>
---
+++ src/pages/open-location/open-location.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="lost">
+    <navigation-bar title="选择自提点" :showArrow="false"></navigation-bar>
+    <div class="img-box">
+      <img :src="imageUrl + '/yx-image/order/pic-address@2x.png'" v-if="imageUrl" class="error-img">
+      <p class="text">亲，我们未获得您的位置信息</p>
+    </div>
+    <navigator class="btn" open-type="switchTab" url="/pages/choiceness">开启定位</navigator>
+    <navigator class="btn select-btn" open-type="switchTab" url="/pages/choiceness">手动选择自提点</navigator>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import NavigationBar from '@components/navigation-bar/navigation-bar'
+
+  const PAGE_NAME = 'LOST'
+
+  export default {
+    name: PAGE_NAME,
+    data() {
+      return {}
+    },
+    onLoad(e) {
+      if (e.shopId) {
+        wx.setStorageSync('shopId', wx.getStorageSync('defaultShopId'))
+      }
+    },
+    components: {
+      NavigationBar
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+
+  .lost
+    padding-top: 22.48vh
+    box-sizing: border-box
+
+  .img-box
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+    height: 138.5px
+    .error-img
+      display: block
+      height: 110px
+      width: 116px
+    .text
+      text-align: center
+      font-size: $font-size-14
+      color: $color-text-sub
+      font-family: $font-family-regular
+      margin-top: 14.5px
+
+  .btn
+    width: 72vw
+    height: 45px
+    line-height: 45px
+    text-align: center
+    background: $color-main
+    border-radius: 50px
+    color: $color-white
+    font-size: $font-size-16
+    margin: 50px auto 0
+    &:active
+      color: #E1F2C9
+      background: #9DD44C
+  .select-btn
+    background: $color-white
+    border-1px($color-text-assist, 50px)
+    color: $color-text-main
+    margin: 20px auto 0
+    &:active
+      color: $color-text-main
+      background: $color-white
+</style>
