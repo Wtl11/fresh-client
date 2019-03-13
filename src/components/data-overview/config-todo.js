8080dcb7977af0e4f191afecdfb5c89911c364d1
jtr <jtr354@foxmail.com>
2019-03-13 18:18:23 +0800

    清除eharts

---
+++ src/components/data-overview/config-todo.js
@@ -0,0 +1,15 @@
+// import Vue from 'vue'
+// import Page from './data-overview'
+
+// const page = new Vue(Page)
+// page.$mount()
+
+export default {
+  config: {
+    navigationBarTitleText: '数据统计'
+    // 这儿添加要用的小程序组件
+    // usingComponents: {
+    //   'ec-canvas': '../../../static/ec-canvas/ec-canvas'
+    // }
+  }
+}
