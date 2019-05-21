6bc553a25f7ec72032a6a635435d4e8c0922685b
guojing <939316942@qq.com>
2019-05-21 20:13:45 +0800

    拼团详情静态页

--- src/pages/collage-detail/collage-detail.vue
+++ src/pages/collage-detail/collage-detail.vue
@@ -1,28 +1,69 @@
 <template>
   <div class="collage-detail">
     <navigation-bar title="拼团详情"></navigation-bar>
-    <p class="top-text">当前社区不支持拼团活动: 国际单位社区</p>
-    <p class="status-text"><img src="" alt="" class="icon">开团成功</p>
-    <p class="status-tip">还差<span class="mark">1</span>人，赶紧邀请好友来拼团吧</p>
-    <p class="run-time">剩余 <span class="time-num">{{timeArr[0]}}</span><span class="time-num">{{timeArr[1]}}</span><span class="time-num">{{timeArr[2]}}</span></p>
-    <div class="heads">
-      <div v-for="(item, index) in headArr" :key="index" class="head-box">
-        <img src="" alt="" class="logo">
+    <p v-if="topText1" class="top-text">该活动仅支持： 国际单位社区</p>
+    <p v-if="topText2" class="top-text">当前社区不支持拼团活动: 国际单位社区</p>
+    <!--商品信息-->
+    <div v-if="goodsBox" class="goods-box">
+      <div class="goods-detail">
+        <img src="" alt="" class="goods-img">
+        <div class="right-content">
+          <p class="title">超值特惠 4斤新鲜柠檬是是三三首饰所所死侍死侍</p>
+          <p class="context">脆嫩爽口，酸甜多汁，口感脆嫩，新鲜美味</p>
+          <div class="marks">
+            <span class="total">200人已拼</span>
+            <span class="count">3人团</span>
+          </div>
+          <p class="price">
+            <span class="money">3.8</span>
+            <span class="unit">元</span>
+          </p>
+        </div>
       </div>
     </div>
-    <button class="btn" open-type="share">邀请好友参团</button>
-    <div class="btn" @click="_initLocation">获取位置信息</div>
-    <p class="bot-tip">分享好友越多，成团越快</p>
-    <div class="line"></div>
-    <div class="goods">
-      <span class="label">拼团商品</span>
-      <span class="text">精品油桃3公斤装，整箱...</span>
+
+    <p v-if="statusText" class="status-text"><img :src="imageUrl + '/yx-image/collage/'+ data.icon +'@2x.png'" alt="" class="icon">{{data.statusText}}</p>
+    <p v-if="statusTip1" class="status-tip">还差<span class="mark">1</span>人，快喊邻居一起来拼团吧</p>
+    <p v-if="statusTip2" class="status-tip">仅剩<span class="mark">1</span>个名额</p>
+    <p v-if="runTime" class="run-time">剩余 <span class="time-num">{{timeArr[0]}}</span>:<span class="time-num">{{timeArr[1]}}</span>:<span class="time-num">{{timeArr[2]}}</span></p>
+
+    <!--头像-->
+    <div class="heads">
+      <div v-for="(item, index) in headArr" :key="index" class="head-box" :class="[{'small-head': headArr.length > 4}, {'has-border': false}]">
+
+        <img :src="imageUrl + '/yx-image/collage/pic-touxiang@2x.png'" alt="" class="logo">
+
+        <span v-if="index === 0" class="tag">拼主</span>
+      </div>
     </div>
-    <div class="time">
+
+    <button class="btn" open-type="share">{{data.btn}}</button>
+    <!--<div class="btn" @click="_initLocation">获取位置信息</div>-->
+    <p v-if="botTip" class="bot-tip">分享邻居越多，成团越快</p>
+    <div v-if="goods || time" class="line"></div>
+    <div v-if="goods" class="goods">
+      <span class="label">拼团商品</span>
+      <p class="text">精品油桃3公斤装，整箱...<img :src="imageUrl + '/yx-image/collage/icon-pressed@2x.png'" class="icon"></p>
+    </div>
+    <div v-if="time" class="time">
       <span class="label">拼团时间</span>
       <span class="text">2019-12-12 12:12:12</span>
     </div>
-    <div class="recommend">
+
+    <!--进度-->
+    <div class="progress-handle">
+      <div class="progress-bg">
+        <span class="progress" :style="{width: (19 + 33.333*step) + '%'}"></span>
+        <span v-for="(item, index) in progress" :key="index" :style="{left: -2 + (index * 33.333) + '%'}" class="step" :class="{'active': step >= index}">{{item}}</span>
+        <span v-for="(item, index) in text" :key="index" :style="{left: -8.5 + (index * 33.333) + '%'}" class="progress-text" :class="{'activity': step >= index}">{{item}}</span>
+      </div>
+    </div>
+
+    <!--分享-->
+    <share-pop ref="shareList"></share-pop>
+
+    <!--猜你喜欢-->
+    <div v-if="recommend" class="recommend">
       <p class="title">
         <img src="https://img.jkweixin.net/defaults/yx-image/2.3/icon-ulike@2x.png" alt="" class="icon">
         <span class="text">猜你喜欢</span>
@@ -41,24 +82,213 @@
   import NavigationBar from '@components/navigation-bar/navigation-bar'
   import ClearWatch from '@mixins/clear-watch'
   import GoodsItem from './goods-item/goods-item'
+  import SharePop from './share-pop/share-pop'
+  import { formatNumber } from '@utils/common'
   import API from '@api'
   const PAGE_NAME = 'COLLAGE_DETAIL'
+
+  const STATUS_ARR = [
+    // 拼主
+    {
+      type: 'master',
+      status: 1,
+      statusText: '开团成功',
+      btn: '邀请邻居参团',
+      step: 1,
+      icon: 'icon-success'
+    },
+    {
+      type: 'master',
+      status: 2,
+      statusText: '待成团',
+      btn: '邀请邻居参团',
+      step: 1,
+      icon: 'icon-success'
+    },
+    {
+      type: 'master',
+      status: 3,
+      statusText: '拼团成功',
+      btn: '查看我的订单',
+      step: 2,
+      icon: 'icon-success'
+    },
+    {
+      type: 'master',
+      status: 4,
+      statusText: '拼团失败',
+      btn: '查看更多拼团商品',
+      step: 1,
+      icon: 'icon-failure'
+    },
+    // 已参团
+    {
+      type: 'customer',
+      status: 2,
+      statusText: '待成团',
+      btn: '邀请邻居参团',
+      step: 1,
+      icon: 'icon-success'
+    },
+    {
+      type: 'customer',
+      status: 5,
+      statusText: '参团成功',
+      btn: '查看我的订单',
+      step: 1,
+      icon: 'icon-success'
+    },
+    {
+      type: 'customer',
+      status: 6,
+      statusText: '拼团成功',
+      btn: '看看别的拼团',
+      step: 2,
+      icon: 'icon-success'
+    },
+    {
+      type: 'customer',
+      status: 4,
+      statusText: '拼团失败',
+      btn: '查看更多拼团商品',
+      step: 1,
+      icon: 'icon-failure'
+    },
+    {
+      type: 'customer',
+      status: 7,
+      statusText: '不在范围',
+      btn: '查看更多拼团商品',
+      step: 1,
+      icon: 'icon-failure'
+    },
+    // 未参与
+    {
+      type: 'customer',
+      status: 8,
+      statusText: '',
+      btn: '一键参团',
+      step: 1,
+      icon: ''
+    },
+    {
+      type: 'customer',
+      status: 9,
+      statusText: '该团已满',
+      btn: '我来开团',
+      step: '',
+      icon: 'icon-end'
+    },
+    {
+      type: 'customer',
+      status: 10,
+      statusText: '该团已结束',
+      btn: '看看别的拼团活动',
+      step: '',
+      icon: 'icon-end'
+    }
+  ]
+
   export default {
     page: PAGE_NAME,
     mixins: [ClearWatch],
     data() {
       return {
+        statusArr: STATUS_ARR,
         headArr: [1, 2],
+        progress: [1, 2, 3, 4],
+        text: ['开团', '邀请邻居', '拼团成功', '拼团返券'],
         orderId: 1,
         distance: true,
         timer: '',
         timeArr: [],
-        recommendList: []
+        recommendList: [],
+        step: STATUS_ARR[0].step,
+        data: STATUS_ARR[0]
+      }
+    },
+    computed: {
+      topText1() { // 开团成功
+        return +this.data.status === 1
+      },
+      topText2() { // 不在范围
+        return +this.data.status === 7
+      },
+      goodsBox() {
+        if (+this.data.status === 2 ||
+          +this.data.status === 3 ||
+          +this.data.status === 4 ||
+          +this.data.status === 8
+        ) {
+          return true
+        }
+        return false
+      },
+      statusText() {
+        if (+this.data.status === 1 ||
+          +this.data.status === 2 ||
+          +this.data.status === 3 ||
+          +this.data.status === 9 ||
+          +this.data.status === 10
+        ) {
+          return true
+        }
+        return false
+      },
+      statusTip1() {
+        if (+this.data.status === 1 ||
+          +this.data.status === 2 ||
+          +this.data.status === 4
+        ) {
+          return true
+        }
+        return false
+      },
+      statusTip2() {
+        return +this.data.status === 7
+      },
+      runTime() {
+        if (+this.data.status === 1 ||
+          +this.data.status === 2 ||
+          +this.data.status === 4 ||
+          +this.data.status === 8
+        ) {
+          return true
+        }
+        return false
+      },
+      botTip() {
+        if (+this.data.status !== 4 && +this.data.status !== 9 && +this.data.status !== 10) {
+          return true
+        }
+        return false
+      },
+      goods() {
+        if (+this.data.status === 1 ||
+          +this.data.status === 3 ||
+          +this.data.status === 4
+        ) {
+          return true
+        }
+        return false
+      },
+      time() {
+        if (+this.data.status !== 9 && +this.data.status !== 10) {
+          return true
+        }
+        return false
+      },
+      recommend() {
+        if (+this.data.status === 9 || +this.data.status === 10) {
+          return true
+        }
+        return false
       }
     },
     components: {
       NavigationBar,
-      GoodsItem
+      GoodsItem,
+      SharePop
     },
     onShareAppMessage() {
       let shopId = wx.getStorageSync('shopId')
@@ -83,12 +313,16 @@
       clearInterval(this.timer)
     },
     onLoad(options) {
+      console.log(options)
       this.timeHandle()
     },
     async onShow() {
       await this.getCarRecommend()
     },
     methods: {
+      showShare() {
+        this.$refs.sharePop.show()
+      },
       timeHandle() {
         let defaultTime = new Date().getTime() + 60 * 60 * 100 * 24
         let nowTime = new Date()
@@ -99,9 +333,9 @@
             clearInterval(this.timer)
           }
           let date = new Date(timeDef)
-          let hours = date.getHours()
-          let minutes = date.getMinutes()
-          let seconds = date.getSeconds()
+          let hours = formatNumber(date.getHours())
+          let minutes = formatNumber(date.getMinutes())
+          let seconds = formatNumber(date.getSeconds())
           this.timeArr = [hours, minutes, seconds]
         }, 1000)
       },
@@ -168,95 +402,243 @@
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~@designCommon"
-  .top-text
-    height: 24px
-    font-size: $font-size-14
-    font-family: $font-family-regular
-    padding: 0 14px
-    line-height: 24px
-    background: #fdf3d5
-  .status-text
-    font-size: $font-size-24
+  .goods-box
+    background: #F7F7F7
+    padding-bottom: 12px
+  .goods-detail
+    padding: 15px 13px
     display: flex
     align-items: center
-    margin-top: 20px
+    background: #FFF
+    .goods-img
+      width: 105px
+      height: 105px
+      background: #eee
+      border-radius: 2px
+    .right-content
+      height: 105px
+      margin-left: 10px
+      flex: 1
+      overflow: hidden
+    .title
+      font-family: $font-family-medium
+      font-size: $font-size-16
+      line-height: 20px
+      color: #111
+      overflow: hidden
+      text-overflow: ellipsis
+      white-space: nowrap
+    .context
+      font-family: $font-family-regular
+      font-size: $font-size-14
+      line-height: 14px
+      color: #808080
+      margin-top: 6px
+      overflow: hidden
+      text-overflow: ellipsis
+      white-space: nowrap
+    .marks
+      margin-top: 8px
+      display: flex
+      align-items: center
+      color: #fa7500
+      font-size: $font-size-12
+      font-family: $font-family-regular
+      .total
+        padding: 2px 7px
+        background: rgba(255,133,6,0.15)
+        border-radius: 2px
+      .count
+        padding: 2px 7px
+        border-radius: 2px
+        margin-left: 5px
+        border-1px(rgba(250,117,0,0.20))
+    .price
+      color: #FA7500
+      margin-top: 10px
+      .money
+        font-family: $font-family-medium
+        font-size: 25px
+        line-height: 25px
+      .unit
+        font-family: $font-family-regular
+        font-size: $font-size-14
+  .top-text
+    height: 40px
+    font-size: 15px
+    font-family: $font-family-regular
+    color: #FF8300
+    padding: 0 12px
+    line-height: 40px
+    background: #FFEBD6
+  .status-text
+    font-family: $font-family-medium
+    color: $color-main
+    font-size: 22px
+    display: flex
+    align-items: center
+    margin-top: 22px
     justify-content: center
     .icon
-      width: 32px
-      height: 32px
+      width: 35px
+      height: 35px
       border-radius: 50%
-      margin-right: 10px
+      margin-right: 4px
       background: #ccc
   .status-tip
-    margin-top: 20px
+    margin-top: 30px
     font-size: $font-size-14
-    font-family: $font-family-regular
+    font-family: $font-family-medium
     text-align: center
+    .mark
+      color: #FA7500
   .run-time
-    text-align: center
+    font-family: $font-family-regular
+    font-size: $font-size-14
+    color: #3F454B
+    line-height: 20px
+    margin-top: 14px
+    display: flex
+    align-items: center
+    justify-content: center
     .time-num
       display: inline-block
-      width: 22px
+      width: 20px
       height: 20px
       text-align: center
       line-height: 20px
-      margin-right: 5px
-      background: #000
-      font-size: $font-size-12
+      border-radius: 4px
+      margin-left: 3px
+      margin-right: 3px
+      background: #3F454B
+      font-size: $font-size-14
+      font-family: $font-family-din-bold
       color: #FFF
+      &:first-child
+        margin-left: 6px
   .heads
-    padding: 0 50px
+    padding: 0 12px
     display: flex
     align-items: center
     justify-content: center
     .head-box
-      width: 40px
-      height: 40px
-      margin-top: 20px
-      margin-right: 20px
+      flex: 0 0 1
+      height: 50px
+      margin: 0 4%
+      margin-top: 28px
       border-radius: 50%
-      overflow: hidden
-      border-1px()
+      position: relative
+    .small-head
+      margin: 0 2%
+    .has-border
+      border: 1px solid #E0E0E0
     .logo
-      width: 100%
-      height: 100%
-      background: #ccc
+      width: 50px
+      height: 50px
+      border-radius: 50%
+    .tag
+      width: 34px
+      height: 14px
+      border-radius: 15px
+      background: #FFD14F
+      row-center()
+      bottom: -7px
+      text-align: center
+      line-height: 14px
+      color: #A03918
+      font-size: 10px
+      font-family: $font-family-medium
   .btn
-    margin: 10px auto
-    width: 80%
+    margin: 0 auto
+    margin-top: 25px
+    width: 270px
     height: 45px
     line-height: 45px
     text-align: center
     color: #FFF
-    font-size: $font-size-14
-    font-family: $font-family-regular
-    border-radius: 8px
-    background: #77dafb
+    font-size: $font-size-16
+    font-family: $font-family-medium
+    border-radius: 50px
+    background: $color-main
   .bot-tip
+    margin-top: 12px
     color: $color-text-assist
-    font-size: $font-size-14
+    font-size: 13px
     font-family: $font-family-regular
     text-align: center
     margin-bottom: 15px
   .line
     height: 10px
-    background: #ccc
+    background: #F7F7F7
 
   .goods,.time
-    height: 34px
+    height: 50px
+    margin: 0 12px
     display: flex
     align-items: center
     justify-content: space-between
-    padding: 0 15px
     font-size: $font-size-14
-    border-bottom-1px()
+    border-bottom-1px(#E6E6E6)
     .label
-      color: #ccc
+      width: 180px
+      overflow: hidden
+      text-overflow: ellipsis
+      white-space: nowrap
+      color: #808080
     .text
-      color: #333
+      color: #111
+      display: flex
+      align-items: center
+    .icon
+      width: 6px
+      height: 12px
+      margin-left: 4px
 
-  .progress
-    padding: 0 14px
+  .progress-handle
+    padding: 30px 12px 60px
+    .progress-bg
+      width: 275px
+      height: 5px
+      background: #CBCAD3
+      position: relative
+      margin: 0 auto
+    .progress
+      width: 0
+      height: 5px
+      background: linear-gradient(90deg, #73C200, #ABE300)
+      border-radius: 5px
+      position: absolute
+      left: 0
+      top: 0
+    .step
+      width: 14px
+      height: 14px
+      border-radius: 50%
+      background: #FFF
+      border: 1px solid #E5E5E5
+      text-align: center
+      line-height: 14px
+      font-family: $font-family-medium
+      color: #808080
+      font-size: $font-size-10
+      position: absolute
+      top: -5px
+      transition: all 0.3s
+    .active
+      background: $color-main
+      color: #FFF
+      border: 1px solid transparent
+    .progress-text
+      font-size: $font-size-12
+      font-family: $font-family-regular
+      color: #808080
+      width: 50px
+      text-align: center
+      position: absolute
+      top: 20px
+      left: 0
+    .activity
+      color: $color-main
   .recommend
     .title
       height: 60px
---
+++ src/pages/collage-detail/share-pop/share-pop.vue
@@ -0,0 +1,91 @@
+<template>
+  <form action="" report-submit @submit="$getFormId">
+    <div class="share-pop">
+      <div scroll-y class="share-box" v-show="show" @touchmove.prevent="" @click.stop="hide"></div>
+      <div class="share-main" :class="show ? 'show' : ''">
+        <p class="tip">拼团已发起，人满后即可发货</p>
+        <div class="content">
+          <p class="title">
+            还差 <span class="color">1</span>人，赶快邀请 <span class="color">国际单位</span>社区邻居来参团吧
+          </p>
+          <button open-type="share" class="share-btn" @click.stop="hide">
+            <img :src="imageUrl + '/yx-image/collage/pic-wechat@2x.png'" alt="" class="img">
+          </button>
+          <p class="text">邀请邻居进度 <span class="color">1/2</span></p>
+        </div>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script type="text/ecmascript-6">
+  const COMPONENT_NAME = 'SHARE_POP'
+
+  export default {
+    name: COMPONENT_NAME,
+    data() {
+      return {
+        show: false
+      }
+    },
+    methods: {
+      hide() {
+        this.show = false
+      },
+      show() {
+        this.show = true
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+ @import "~@designCommon"
+
+  .share-box
+    position: fixed
+    left: 0
+    top: 0
+    bottom: 0
+    right: 0
+    z-index: 100
+    background: rgba(55, 75, 99, 0.8)
+  .share-main
+    position: fixed
+    bottom: -130%
+    left: 0
+    width: 100%
+    z-index: 150
+    transition: all .5s
+    .tip
+      font-family: $font-family-regular
+      font-size: $font-size-14
+      color: #FFF
+      opacity: 0.8
+      text-align: center
+    .content
+      box-sizing: border-box
+      margin-top: 12px
+      padding: 20px 60px 30px
+      width: 100%
+      background: #FFF
+    .title
+      font-family: $font-family-medium
+      font-size: $font-size-16
+      color: #111
+      line-height: 24px
+      text-align: center
+    .color
+      color: #FA7500
+    .img
+      width: 50px
+      height: 50px
+      margin: 30px auto 10px
+    .text
+      text-align: center
+      font-size: $font-size-14
+      color: #1D2023
+      font-family: $font-family-regular
+  .show
+    bottom: 0
+</style>
