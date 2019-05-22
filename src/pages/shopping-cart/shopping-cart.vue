556f6e1c6545938316bb67ae8765513c2ddcbd6f
liujiajie <liujiajie@jkweixin.net>
2019-05-22 17:02:20 +0800

    2.4 购物车接口字段修改

--- src/pages/shopping-cart/shopping-cart.vue
+++ src/pages/shopping-cart/shopping-cart.vue
@@ -29,7 +29,7 @@
               <div class="price" :class="'corp-' + corpName + '-money'" v-if="item.trade_price">
                 <span class="num">{{item.trade_price}}</span>
                 <span class="unit">元</span>
-                <img class="new-user-img" v-if="imageUrl && item.activity && item.activity.activity_type === 'new_client'" :src="imageUrl + '/yx-image/2.4/pic-newlabel@2x.png'" alt="">
+                <img class="new-user-img" v-if="imageUrl && item.is_new_client === 1 && item.activity && item.activity.activity_type === 'new_client'" :src="imageUrl + '/yx-image/2.4/pic-newlabel@2x.png'" alt="">
               </div>
             </div>
             <div class="right">
