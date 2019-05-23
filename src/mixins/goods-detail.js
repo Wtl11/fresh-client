62d6189fe56fe7dc9e99cb392649ff2ae16d36c2
jtr <jtr354@foxmail.com>
2019-05-23 21:53:49 +0800

    团购主流程

--- src/mixins/goods-detail.js
+++ src/mixins/goods-detail.js
@@ -13,7 +13,6 @@ export default {
     // 获取地理位置
     async _getLocation() {
       if (this.activityType !== ACTIVE_TYPE.GROUP_ON) return
-      console.log(ACTIVE_TYPE)
       if (this.latitude && this.longitude) return
       try {
         let res = await this.$wechat.getLocation()
@@ -23,12 +22,13 @@ export default {
           wx.navigateTo({url: `/pages/open-location`})
         } else {
           res = await API.Global.checkShopDistance({longitude: this.longitude, latitude: this.latitude})
-          this.tipTop = res.distance_judge === 0 ? `当前位置${res.data.shop.social_name}社区不参与拼团活动` : ''
+          let name = res.data.shop.social_name
+          name = name.substring(0, 6) + (name.length > 6 ? '...' : '')
+          this.tipTop = res.data.distance_judge === 0 ? '' : `当前位置${name}社区不参与拼团活动`
         }
       } catch (e) {
         wx.navigateTo({url: `/pages/open-location`})
       }
-      console.log(this.longitude, this.latitude)
     }
   }
 }
