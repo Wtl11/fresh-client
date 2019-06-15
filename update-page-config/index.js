9b9ce50d687cd6e094a50b200a953fe97815f8c5
jtr <jtr354@foxmail.com>
2019-06-15 15:06:04 +0800

    优化page.json

--- update-page-config/index.js
+++ update-page-config/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
-// const filePath = path.join(__dirname, '../src/pages/main/')
+const filePath = path.join(__dirname, '../src/pages/main/')
 // const filePath = path.join(__dirname, '../src/pages/package-leader/')
-const filePath = path.join(__dirname, '../src/pages/unuse-page/')
+// const filePath = path.join(__dirname, '../src/pages/unuse-page/')
 
 fs.readdir(filePath, (err, files) => {
   if (err) {
@@ -20,9 +20,8 @@ fs.readdir(filePath, (err, files) => {
       const context = fs.readFileSync(configPath, {encoding: 'utf-8'})
       if (!/export default /.test(context)) return
       fs.writeFileSync(pagePath + '/test.js', 'module.exports = ' + context.split('export default ')[1])
-      const f = require(pagePath + '/test.js')
-      let json = (JSON.stringify(f.config)).replace(/'/g, '"')
-      fs.writeFileSync(pagePath + `/${item}.json`, json)
+      const tmpJson = require(pagePath + '/test.js')
+      fs.writeFileSync(pagePath + `/${item}.json`, JSON.stringify(tmpJson.config, null, 2))
       fs.unlinkSync(pagePath + '/test.js')
     } catch (e) {
       console.log(index, item)
