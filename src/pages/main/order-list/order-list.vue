da665a6bd73fa5f06a06f01e2a48a952923dc19a
jtr <jtr354@foxmail.com>
2019-08-21 11:36:50 +0800

    晒单bug

--- src/pages/main/order-list/order-list.vue
+++ src/pages/main/order-list/order-list.vue
@@ -64,7 +64,7 @@
               <div class="bot">
                 <div class="time">{{item.created_at}}</div>
                 <!--<div class="payment"><span class="goods-count">共{{item.goods.length}}件商品</span><span class="actual">总计：</span><span class="sum">{{item.total}}</span><span class="principal">元</span></div>-->
-                <button v-if="!item.groupon.id && item.status!==0 && item.status!==3" :id="item.order_id" :shopId="item.shop_id" open-type="share" class="share-btn" @click.stop="">晒单</button>
+                <button v-if="!item.groupon.id && item.status!==0 && item.status!==3" :id="item.order_id + '&'+ item.shop_id " open-type="share" class="share-btn" @click.stop="">晒单</button>
               </div>
             </div>
           </div>
@@ -132,10 +132,12 @@
     },
     onShareAppMessage(e) {
       // shopId待接口增加返回参数
-      let shopId = e.target.shopId || 0
-      let nickname = wx.getStorageSync('userInfo').nickname
+      const params = e.target.id.split('&')
+      const id = params[0]
+      const shopId = params[1]
+      const nickname = wx.getStorageSync('userInfo').nickname
       const flag = Date.now()
-      console.log(shopId)
+      console.info(`${this.$routes.main.SHARE_ORDER}?id=${id}&shopId=${shopId}&flag=${flag}`)
       return {
         title: `团长，我是“${nickname}”，刚在店里买了商品↓，请接单！`,
         path: `${this.$routes.main.SHARE_ORDER}?id=${e.target.id}&shopId=${shopId}&flag=${flag}`,
